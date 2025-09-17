937f599dd7b46ce293884605b19531890e3e8874
Deep245 <111187268+Deep245@users.noreply.github.com>
2025-09-17 10:17:54 +0200

    Delete OD_SlidingWindow directory

--- OD_SlidingWindow/clusteringInit.m
+++
@@ -1,8 +0,0 @@
-    % Get static points for this frame within the sliding window
-    static_points_window = [];
-    static_labels = [];
-    idxS = [];
-    % Get dynamic points for this frame within the sliding window
-    dynamic_points_window = [];
-    dynamic_labels = [];
-    idxD = [];
